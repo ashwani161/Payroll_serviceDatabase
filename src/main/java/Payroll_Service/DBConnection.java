34b7bfe41a07293a277483f3ae3f3074eef9d48b
ashwani161 <ashwanin65@gmail.com>
2021-04-15 12:32:47 +0530

    Added:Create payrollService Database and have  java program connection to database

--- src/main/java/Payroll_Service/DBConnection.java
+++ src/main/java/Payroll_Service/DBConnection.java
@@ -1,8 +1,24 @@
 package Payroll_Service;
 
-public class DBConnection {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Mysql Database Connection");
-	}
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class DBConnection {
+	//public static void main(String[] args) {
+	 String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service";
+	 String userName = "root";
+	 String password = "Ashu@8541";
+		Connection connection;
+		public Connection getConnection() {
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				Connection connection = DriverManager.getConnection(jdbcURL, userName, password);
+				return connection;
+			} catch (ClassNotFoundException | SQLException e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+		//System.out.println("Welcome to Mysql Database Connection");
 }
